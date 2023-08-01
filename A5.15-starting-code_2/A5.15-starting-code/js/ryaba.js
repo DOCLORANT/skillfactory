bf16d7f383e835eeb395c0dc4894e51eda7f60d8
DOCDR <drauzinanton@gmail.com>
2023-08-01 12:56:24 +1000

    Добавил папку с домашним заданием курочка ряба из модуля A5.15

---
+++ A5.15-starting-code_2/A5.15-starting-code/js/ryaba.js
@@ -0,0 +1,33 @@
+const dataURL = "https://api.jsonbin.io/b/5e8c9fceff9c906bdf1d8635";
+
+function handleButton() {
+  // взять данные по dataUrl, вытащить их и передать в handleData
+}
+
+function handleData(data) {
+  // кажется, какой-то из этих способов сработает
+  //const var1 = $("input[name=var1]")[0].value()
+  //const var1 = $("input[name=var1]").value()
+  //const var1 = $("input[name=var1]")[0].default()
+
+  // надо сделать так чтобы сообщения подменились на значения из формы
+  let text = "Здесь могла быть ваша реклама";
+	$("#result").text(text);
+}
+
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
+
+/*
+var1 - дед
+var2 - баба
+var3 - курочка ряба
+var4 - яичко
+var5 - мышка
+var6 - хвостиком
+speach - Не плачь, дед, не плачь, 
+баба: снесу вам яичко не золотое - простое!
+ */
